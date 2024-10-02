596efcbc3f78ad53ba363fc149ae3c5bc45ba44b
Serhii Bets <108925503+serhiibets@users.noreply.github.com>
2024-10-02 18:21:59 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ import PackageDescription
 let package = Package(
     name: "AppRouter",
     platforms: [
-        .iOS(.v10)  // Set the minimum iOS version, adjust if needed
+        .iOS(.v14)
     ]
 )
